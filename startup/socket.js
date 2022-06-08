1a863bc4b21b7a4fe28801465edba1b2c9a295c9
Lee Taylor <leetaylor5455@yahoo.co.uk>
2022-06-08 16:33:01 +0100

    Heartbeat WS To Eliminate Idle Connections

--- startup/socket.js
+++ startup/socket.js
@@ -3,16 +3,23 @@ const Game = require('../models/game');
 
 exports.startupSocket = function(server) {
 
+    function heartbeat() {
+        this.isAlive = true;
+    }
+
     const wss = new WebSocketServer({ server });
 
     wss.broadcast = function(data) {
-        console.log('broadcast')
         wss.clients.forEach(function each(client) {
             client.send(JSON.stringify(data));
         });
     }
 
     wss.on('connection', function connection(ws) {
+
+        ws.isAlive = true;
+        ws.on('pong', heartbeat);
+
         ws.on('message', async function message() {
             
             const game = await Game.findOne({});
@@ -20,6 +27,19 @@ exports.startupSocket = function(server) {
             wss.broadcast(game);
         });
     });
+
+    const interval = setInterval(function ping() {
+        wss.clients.forEach(function each(ws) {
+            if (ws.isAlive === false) return ws.terminate();
+
+            ws.isAlive = false;
+            ws.ping();
+        });
+    }, 20000);
+
+    wss.on('close', function close() {
+        clearInterval(interval);
+    })
 
     exports.wss = wss;
 }
